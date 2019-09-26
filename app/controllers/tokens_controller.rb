ccccd04ef486ba57d403630756abb41357ce2586
Joan Racenet <joan.racenet@hiptest.net>
2019-09-26 18:30:46 +0200

    Move create (or find) user based on GH token in a service

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -4,36 +4,13 @@ class TokensController < ApplicationController
   include HTTParty
 
   def create
-    token = get_access_token
-    user_info = get_current_user_profile(token)
-    user = User.from_github(user_info, token)
+    service = GithubAccessGetterService.new(params)
+    user = service.get_or_create_current_user
 
     render json: payload(user), status: :ok
   end
 
   private
-
-  def get_current_user_profile(token)
-    client = Octokit::Client.new(access_token: token)
-    client.user
-  end
-
-  def get_access_token
-    res = HTTParty.post('https://github.com/login/oauth/access_token',{
-      headers: {
-        'Accept' => 'application/json'
-      },
-      body: {
-        client_id:  "7f026a219b30b86d64fa",
-        client_secret: "615eb56bff46c7d6a6e1b0e5971851c400e24885",
-        code: params['code'],
-        grant_type: 'authorization_code',
-        redirect_uri: "http://localhost:4200/oauth2"
-      }
-    })
-
-    JSON.parse(res.body)["access_token"]
-  end
 
   def payload(user)
     {
