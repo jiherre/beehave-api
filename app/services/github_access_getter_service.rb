97bac0edb694896ff3550260e0bc82ef3ffb4415
Joan Racenet <joan.racenet@hiptest.net>
2019-09-27 16:36:23 +0200

    Fix github access getter service

--- app/services/github_access_getter_service.rb
+++ app/services/github_access_getter_service.rb
@@ -2,7 +2,7 @@ require 'octokit'
 
 class GithubAccessGetterService
   def initialize(params)
-    @temp_token = params[:code]
+    @temp_token = params['code']
   end
 
   def get_or_create_current_user
@@ -21,7 +21,7 @@ class GithubAccessGetterService
       body: {
         client_id:  "7f026a219b30b86d64fa",
         client_secret: "615eb56bff46c7d6a6e1b0e5971851c400e24885",
-        code: params['code'],
+        code: @temp_token,
         grant_type: 'authorization_code',
         redirect_uri: "http://localhost:4200/oauth2"
       }
